3f9f3c407e1ef7ba5e81c8318facb0f9af033e0d
rickscole <ricksaubercole@gmail.com>
2025-07-04 14:35:16 -0400

    Create create_krn_recap.sql

---
+++ kronokol/create_krn_recap.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [krn].[recap](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[ts] [datetime2](7) NULL,
+	[start_date] [datetime2](7) NULL,
+	[end_date] [datetime2](7) NULL,
+	[value] [nvarchar](4000) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [krn].[recap] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
